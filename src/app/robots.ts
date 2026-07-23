c7c700f12bf9f345884973f6c22d7ac840d8f5ae
chris0306chen <chris0306chen@gmail.com>
2026-07-23 16:33:41 +0800

    Fix production www canonical URLs

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = (process.env.NEXT_PUBLIC_SITE_URL || 'https://www.reachprojector.com').replace(/\/$/, '')
+  const siteUrl = 'https://www.reachprojector.com'
 
   return {
     rules: {
